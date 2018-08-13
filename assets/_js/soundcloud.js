2e9f34953a4559c7e8fc40e71f842174a1991806
Leeza <leeza@iLabs-iMac.local>
2018-08-13 15:18:07 -0400

    different soundcloud library

--- assets/_js/soundcloud.js
+++ assets/_js/soundcloud.js
@@ -1,18 +1,75 @@
-import { SC } from 'sdk'
-export function player() {
-  console.log('player')
-  SC.initialize({
-    client_id: 'e1b9039f824fdaf6ec1fc594037c1ac7'
-  })
-  let URL =
-    'https://soundcloud.com/csis-57169780/are-tariffs-the-greatest?in=csis-57169780/sets/the-trade-guys'
+// import * as SoundCloudAudio from 'soundcloud-audio'
+//
+// export default SoundCloudAudio
 
-  SC.resolve(URL).then(json => {
-    let trackPath = `tracks/${json.id}`
-    console.log(trackPath)
-    SC.stream(trackPath).then(player => {
-      console.log(player)
-      player.play()
+import * as SoundCloudAudio from 'soundcloud-audio'
+
+export function player() {
+  let currentPlayer, scPlayer
+  const init = (URL, lengthDisplay) => {
+    scPlayer = new SoundCloudAudio(`{{site.soundcloud_client_id}}`)
+
+    scPlayer.resolve(URL, function(track) {
+      console.log(track.duration, lengthDisplay)
     })
+  }
+
+  // const millisToMinutesAndSeconds = millis => {
+  //   const minutes = Math.floor(millis / 60000)
+  //   const seconds = ((millis % 60000) / 1000).toFixed(0)
+  //   return minutes + ':' + (seconds < 10 ? '0' : '') + seconds
+  // }
+
+  const progressBar = progressDisplay => {
+    if (currentPlayer.isPlaying()) {
+      let progress =
+        (currentPlayer.currentTime() / currentPlayer.getDuration()) * 100
+      console.log(progress)
+      progressDisplay.style.width = `${progress}%`
+    }
+  }
+
+  const setCurrent = (URL, lengthDisplay, progressDisplay) => {
+    scPlayer.initialize({
+      client_id: `{{site.soundcloud_client_id}}`
+    })
+
+    scPlayer.resolve(URL).then(json => {
+      let trackPath = `tracks/${json.id}`
+      scPlayer.stream(trackPath).then(player => {
+        currentPlayer = player
+        player.play()
+        progressBar(progressDisplay)
+      })
+    })
+  }
+
+  document.querySelectorAll('.player').forEach(player => {
+    let URL = player.getElementsByClassName('audio-control')[0].dataset.url
+    let lengthDisplay = player.getElementsByClassName('length')[0]
+    let progressDisplay = player.getElementsByClassName('progress')[0]
+
+    player.querySelectorAll('.audio-control, .status').forEach(element => {
+      element.addEventListener('click', () => {
+        let control = document.querySelector('.audio-control')
+        control.classList.toggle('icon-pause')
+        control.classList.toggle('icon-play')
+
+        let status = document.querySelector('.status')
+        status.textContent === 'LISTEN'
+          ? (status.textContent = 'PLAYING')
+          : (status.textContent = 'LISTEN')
+
+        if (currentPlayer && currentPlayer.isPlaying()) {
+          currentPlayer.pause()
+        } else if (currentPlayer && !currentPlayer.isPlaying()) {
+          currentPlayer.play()
+          progressBar(progressDisplay)
+        } else {
+          setCurrent(URL, lengthDisplay, progressDisplay)
+        }
+      })
+    })
+    init(URL, lengthDisplay, progressDisplay)
   })
 }
