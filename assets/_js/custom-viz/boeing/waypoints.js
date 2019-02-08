91567c6902f04ba4eebba5c63d31036162df935c
Anne Lee Steele <aleesteele@gmail.com>
2019-02-08 11:41:25 -0500

    starting waypoints

--- assets/_js/custom-viz/boeing/waypoints.js
+++ assets/_js/custom-viz/boeing/waypoints.js
@@ -1,7 +1,63 @@
 const Waypoint = require('waypoints/lib/noframework.waypoints.js')
 
 const WaypointsJS = () => {
+  console.log('testing waypoint')
+  let wrapper = document.getElementById('scroll-wrapper')
+  let text = document.getElementByClass('text-scroll')
 
+  //Everything commented out is pseudo-code.
+  let initPlane = new Waypoint.Sticky({
+    element: wrapper,
+    handler: function(direction) {
+      //Ignore below; just test code
+      if (direction == 'down') {
+        console.log('DOWN: going' + direction)
+      } else if (direction == 'up') {
+        console.log('UP: going' + direction)
+      }
+      //This handler should enable the plane element to stick at the top of the page (on mobile), and occupy the full screen (on desktop)
+      //After the scrolling visualization ends, the plane should fade out.
+    }
+  })
+
+  let initText = new Waypoint({
+    element: text,
+    //Because specific functions are triggered with specific textboxes, I think it makes the most sense to have separate waypoint handlers for each text box.
+    //In other words: with the first textbox of the visualization (#s1) reaches its waypoint, this triggers the forward fuselage and forward landing gear to increase to full opacity, while the rest of the plane remains at a lower level of transparency.
+    //TLDR: When user arrives at each waypoint ---> Change in SVG is triggered
+    //Note: When there are multiple pieces mentioned in the paragraph, those pieces are emphasized in succession, with delayed transitions to mimic how the reader would view the information
+    //Total Text Boxes: 9 (So, the number of waypoint functions for the respective paragraphs should also be 9!)
+    enter: function(direction) {
+      //When textbox enters viewpoint
+      console.log(direction)
+    },
+    entered: function(direction) {
+      //When inside viewpoint: trigger animations
+      console.log(direction)
+    },
+    exit: function(direction) {
+      //Pieces fade to less opacity
+      console.log(direction)
+    },
+    exited: function(direction) {
+      //Full opacity plane
+      console.log(direction)
+    }
+  })
+
+  //Waypoint for Map (?)
+  //Triggers full-screen map to appear on screen at the end of the scrollytelling visualization
+  //The waypoint could also trigger the CSS animations, which will likely be animated lines moving towards Everett, WA (where the 787s are manufactured)
+  let initMap = new Waypoint({
+    element: text,
+    handler: function(direction) {
+      if (direction == 'down') {
+        console.log('DOWN: going' + direction)
+      } else if (direction == 'up') {
+        console.log('UP: going' + direction)
+      }
+    }
+  })
 }
 
 export default WaypointsJS
