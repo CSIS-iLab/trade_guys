2583f7a0459c6069d28946f55de5e6ebb048ec27
aleesteele <aleesteele@gmail.com>
2019-02-12 08:57:05 -0500

    config

--- assets/_js/custom-viz/boeing/waypoints.js
+++ assets/_js/custom-viz/boeing/waypoints.js
@@ -1,18 +1,43 @@
-import $ from 'jquery'
-import 'waypoints/lib/jquery.waypoints.min.js'
+import 'waypoints/lib/noframework.waypoints.min'
 
 const WaypointsJS = () => {
   console.log('testing waypoint')
 
-  $('#scroll-wrapper').waypoint(function(direction) {
-    if (direction == 'down') {
-      $('.scroll-wrapper').addClass('full-screen')
-      $('.plane').addClass('sticky')
-    }
-    if (direction == 'up') {
-      console.log('UP: going' + direction)
+  let loadScrollytelling = new Waypoint({
+    element: document.getElementById('scroll-wrapper'),
+    handler: function(direction) {
+      if (direction == 'down') {
+        document.getElementById('scroll-wrapper').classList.add('full-screen')
+        console.log('scroll wrapper entered screen')
+
+        document.getElementById('plane').classList.add('sticky')
+        console.log('plane is full screen')
+      }
     }
   })
+
+  let loadText = function() {
+    let firstWaypoint = document.getElementById('s1')
+    console.log('loadText function')
+    firstWaypoint.waypoint(function() {
+      console.log('arrived at first waypoint')
+    })
+  }
+
+  let loadMap = new Waypoint({
+    element: document.getElementById('map'),
+    handler: function(direction) {
+      if (direction == 'down') {
+        console.log('DOWN: going' + direction)
+      } else if (direction == 'up') {
+        console.log('UP: going' + direction)
+      }
+    }
+  })
+
+  loadScrollytelling()
+  loadText()
+  loadMap()
 }
 
 export default WaypointsJS
