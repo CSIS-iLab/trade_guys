b37764c4b849aec9659247f9bab96660f5d9b9eb
aleesteele <aleesteele@gmail.com>
2019-02-14 13:38:47 -0500

    mobile scroll

--- assets/_js/custom-viz/boeing/waypoints.js
+++ assets/_js/custom-viz/boeing/waypoints.js
@@ -13,32 +13,32 @@ const Waypoints = () => {
   let label1 = document.getElementById('forward_fuselage_label')
   let label2 = document.getElementById('landing_gear_label')
 
-  // let text2 = document.getElementById('s2')
-  // let forward_cargo_doors = document.getElementById('Cargo_access_doors')
-  // let label3 = document.getElementById('Cargo_access_doors_label')
-  //
-  // let text3 = document.getElementById('s3')
-  // let midForwardFuselage = document.getElementById('Mid-forward_fuselage')
-  // let centerWingBox = document.getElementById('Center_wing_box')
-  // let wingBodyFairings = document.getElementById('Wing_body_fairing_landing_gear_doors')
-  // let label4 = document.getElementById('Mid-forward_fuselage_label')
-  // let label5 = document.getElementById('Center_wing_box_label')
-  // let label6 = document.getElementById('Center_fuselage_label')
-  // let label7 = document.getElementById('Wing_body_fairing_landing_gear_doors_label')
+  let text2 = document.getElementById('s2')
+  let forward_cargo_doors = document.getElementById('cargo_access_doors')
+  let label3 = document.getElementById('cargo_access_doors_label')
 
-  // let text4 = document.getElementById('s4')
-  // let wings = document.getElementById('wings')
-  //fixed and moveable leading front edge//
-  // tailing edge
-  //fixed tailing edge
+  let text3 = document.getElementById('s3')
+  let midForwardFuselage = document.getElementById('mid-forward_fuselage')
+  let centerWingBox = document.getElementById('center-wing-box')
+  let wingBodyFairings = document.getElementById('wing-body-fairing-landing-gear-doors')
+  let label4 = document.getElementById('mid-forward-fuselage-label')
+  let label5 = document.getElementById('center-wing-box-label')
+  let label6 = document.getElementById('center_fuselage_label')
+  let label7 = document.getElementById('wing-body-fairing-landing-gear-doors-label')
 
-  // let text5 = document.getElementById('s5')
-  //engines//nacelles
+  let text4 = document.getElementById('s4')
+  let wings = document.getElementById('wings')
+  fixed and moveable leading front edge//
+  tailing edge
+  fixed tailing edge
 
-  // let text6 = document.getElementById('s6')
-  // landing gear//wheels
+  let text5 = document.getElementById('s5')
+  engines//nacelles
 
-  // let text7 = document.getElementById('s7')
+  let text6 = document.getElementById('s6')
+  landing gear//wheels
+
+  let text7 = document.getElementById('s7')
 
   let loadScrollytelling = new Waypoint.Inview({
     element: wrapper,
@@ -59,9 +59,6 @@ const Waypoints = () => {
       console.log('plane is at top of window: ' + direction)
       if (direction == 'down') {
         plane_wrapper.classList.add('fixed')
-        text1.classList.add('visible')
-        label1.classList.add('visible')
-        label2.classList.add('visible')
       } else if (direction == 'up') {
         text1.classList.add('fade-out')
       }
